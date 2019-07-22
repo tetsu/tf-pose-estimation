8c149bfabb4b84c778dd7b6d4ccc6be38dd0be6f
tetsu <tetsuromori@gmail.com>
2019-07-22 15:17:15 +0900

    add 3d estimation

---
+++ run_3d.py
@@ -0,0 +1,73 @@
+import argparse
+import ast
+import logging
+import sys
+import time
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tf_pose import common
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
+
+from lifting.prob_model import Prob3dPose
+from lifting.draw import plot_pose
+
+logger = logging.getLogger('TfPoseEstimator')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-pose-estimation run')
+    parser.add_argument('--image', type=str, default='./images/p1.jpg')
+    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
+    parser.add_argument('--scales', type=str, default='[None]', help='for multiple scales, eg. [1.0, (1.1, 0.05)]')
+    args = parser.parse_args()
+    scales = ast.literal_eval(args.scales)
+
+    w, h = model_wh(args.resolution)
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+
+    # estimate human poses from a single image !
+    image = common.read_imgfile(args.image, None, None)
+    # image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
+    t = time.time()
+    # humans = e.inference(image, upsample_size=scales)
+    humans = e.inference(image, resize_to_default=True, upsample_size=4.0)
+    elapsed = time.time() - t
+
+    logger.info('inference image: %s in %.4f seconds.' % (args.image, elapsed))
+
+    image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+
+    logger.info('3d lifting initialization.')
+    poseLifting = Prob3dPose('./lifting/models/prob_model_params.mat')
+
+    image_h, image_w = image.shape[:2]
+    standard_w = 640
+    standard_h = 480
+
+    pose_2d_mpiis = []
+    visibilities = []
+    for human in humans:
+        pose_2d_mpii, visibility = common.MPIIPart.from_coco(human)
+        pose_2d_mpiis.append([(int(x * standard_w + 0.5), int(y * standard_h + 0.5)) for x, y in pose_2d_mpii])
+        visibilities.append(visibility)
+
+    pose_2d_mpiis = np.array(pose_2d_mpiis)
+    visibilities = np.array(visibilities)
+    transformed_pose2d, weights = poseLifting.transform_joints(pose_2d_mpiis, visibilities)
+    pose_3d = poseLifting.compute_3d(transformed_pose2d, weights)
+
+    for i, single_3d in enumerate(pose_3d):
+        plot_pose(single_3d)
+    plt.show()
+    pass
